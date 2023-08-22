a15b51f08ed7bb5965094ad447a259b3b96cf62b
Raghav Mehta <raghav23mehta@gmail.com>
2023-08-22 20:56:32 +0530

    WIP

--- flink.py
+++ flink.py
@@ -1,2 +1,29 @@
+from pyflink.common import Types
 from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream.connectors.kafka import KafkaSource, KafkaOffsetsInitializer
+from pyflink.common import WatermarkStrategy, SimpleStringSchema
+import logging, sys
 
+def read_from_kafka(env):
+    source = KafkaSource.builder() \
+    .set_bootstrap_servers("localhost:9092") \
+    .set_topics("input-topic") \
+    .set_group_id("my-group") \
+    .set_starting_offsets(KafkaOffsetsInitializer.earliest()) \
+    .set_value_only_deserializer(SimpleStringSchema()) \
+    .build()
+
+    # env.from_source(source, WatermarkStrategy.no_watermarks(), "Kafka Source")
+    env.add_source(source).print()
+    env.execute()
+
+if __name__ == '__main__':
+    # logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
+
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
+    env.add_jars("file:///Users/Raghav/Desktop/DaftPunk/Resources/flink-sql-connector-kafka-1.17.1.jar")
+
+    print("start reading data from kafka")
+    read_from_kafka(env)
+    env.add_source
