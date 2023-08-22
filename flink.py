0b30f01aaa67738ccf4a08021801180c18f2af9a
Raghav Mehta <raghav23mehta@gmail.com>
2023-08-22 21:06:23 +0530

    Two JAR files location added

--- flink.py
+++ flink.py
@@ -7,7 +7,7 @@ import logging, sys
 def read_from_kafka(env):
     source = KafkaSource.builder() \
     .set_bootstrap_servers("localhost:9092") \
-    .set_topics("input-topic") \
+    .set_topics("mqtt-replay") \
     .set_group_id("my-group") \
     .set_starting_offsets(KafkaOffsetsInitializer.earliest()) \
     .set_value_only_deserializer(SimpleStringSchema()) \
@@ -23,7 +23,8 @@ if __name__ == '__main__':
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_parallelism(1)
     env.add_jars("file:///Users/Raghav/Desktop/DaftPunk/Resources/flink-sql-connector-kafka-1.17.1.jar")
+    # env.add_jars("file:///Users/karanbawejapro/Desktop/jarfiles/flink-sql-connector-kafka-1.17.1.jar")
 
-    print("start reading data from kafka")
+    print("Start reading data from kafka: ")
     read_from_kafka(env)
     env.add_source
